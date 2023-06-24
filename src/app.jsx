a74b1e1ad9b0c96055e80fa5490416c4889da016
Adrian Zywno <adria@MacBook-Air.local>
2023-06-23 21:33:05 -0400

    First checkpoint with Tic Tac Toe

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-
 const App = () => {
-  return (
-    <div>
+  const [charXO, setCharXO] = useState('X');
+
+  const handleSwitch = () => {
+    setInterval(function() {
+      if (charXO === 'X') {
+        return setCharXO('O');
+      }
+      else {
+        return setCharXO('X');
+      }
+    }, 300);
+  };
+
+  return(
+
+    <div className='container'>
       <h1>Tic Tac Toe</h1>
+      <Box 
+        handleSwitch={handleSwitch}
+      />
     </div>
   );
-}
+};
 
+const Box = (props) => {
+  return (
+    <button className='square' onClick={props.handleSwitch}>{props.handleSwitch}</button>
+  );
+};
 const root = createRoot(document.querySelector('#root'));
-root.render(<App />,);
+root.render(<App/>,);
